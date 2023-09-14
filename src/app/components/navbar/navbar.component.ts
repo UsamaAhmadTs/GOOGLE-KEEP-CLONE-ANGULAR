226ccbd9fda7467d63fee9ba2b66a045768763bc
UsamaAhmadTs <usama.ahmad@techno-soft.com>
2023-09-14 20:58:12 +0500

    First Commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  @ViewChild("form23zSaZ") form23zSaZ?: ElementRef<HTMLDivElement>
+
+
+
+  refresh() { window.location.reload() }
+
+  ngOnInit(): void { }
+
+}
